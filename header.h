f3d7235443bf0042e7a91d869cba39372d90ed40
rozatunnabila <rozatun24@mhs.usk.ac.id>
2025-03-09 13:01:57 +0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,9 @@
+nanti aku lengkapin tunggu nama fungsii
+
+
+// Deklarasi struktur data untuk nasabah
+typedef struct Nasabah {
+    char nama[50];
+    char layanan[30];
+    struct Nasabah* next; 
+} Nasabah;
