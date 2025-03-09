efb51ea4c49af5964b4f858f9e9abb7af653ec76
rozatunnabila <rozatun24@mhs.usk.ac.id>
2025-03-09 13:04:06 +0700

    Update header.h

--- header.h
+++ header.h
@@ -7,3 +7,18 @@ typedef struct Nasabah {
     char layanan[30];
     struct Nasabah* next; 
 } Nasabah;
+
+// Struktur data untuk antrian pake (Queue)
+typedef struct {
+  // Pointer ke nasabah di depan antrian
+    Nasabah* depan;
+// Pointer ke nasabah di belakang antrian
+    Nasabah* belakang; 
+} Antrian;
+
+// Struktur data untuk Riwayat Transaksi (Stack)
+typedef struct {
+    Nasabah* atas; 
+    // yng ini aku buat biar  kalian nnti terapinnya pake stack jadi ambil riwayat tu lewt yng paling atas dulu baru kalian paggil di fungsi.. biarin gini aja dlu nnti aku rapihin
+} Riwayat;
+
